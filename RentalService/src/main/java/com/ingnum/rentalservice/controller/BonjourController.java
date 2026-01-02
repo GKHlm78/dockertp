3110c653ecb54d8a7831ec5d779ff94b469a0cd4
battlcall <danny.khalfh@yahoo.fr>
2026-01-02 20:06:39 +0100

    ajout service php fonctionnel et docker compose

--- RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -2,12 +2,19 @@ package com.ingnum.rentalservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class BonjourController {
 
+        private final RestTemplate restTemplate = new RestTemplate();
+
+
     @GetMapping("/bonjour")
     public String bonjour() {
-        return "bonjour";
+        String prenom = restTemplate.getForObject("http://php-service:80", String.class);
+        return "bonjour," + prenom;
     }
 }
+
